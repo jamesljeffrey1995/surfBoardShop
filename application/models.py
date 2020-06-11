33545bbba3d163dbb4964b5bc10cf962e7f0e54c
James Jeffrey <jamesljeffrey1995@gmail.com>
2020-06-11 14:48:47 +0000

    User can now add boards to the system

--- application/models.py
+++ application/models.py
@@ -12,7 +12,6 @@ class Users(db.Model, UserMixin):
     last_name = db.Column(db.String(30), nullable=False)
     email = db.Column(db.String(150), nullable=False, unique=True)
     password = db.Column(db.String(500), nullable=False)
-    posts = db.relationship('Posts', backref='author', lazy=True)
 
     def __repr__(self):
         return ''.join([
@@ -21,29 +20,12 @@ class Users(db.Model, UserMixin):
             'Name: ', self.first_name, ' ', self.last_name
         ])
 
-class Posts(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
-    title = db.Column(db.String(100), nullable=False, unique=True)
-    content = db.Column(db.String(500), nullable=False, unique=True)
-
-    def __repr__(self):
-        return ''.join([
-            'User ID: ', self.user_id, '\r\n',
-            'Title: ', self.title, '\r\n', self.content
-        ])
 
 class Order(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     date_ordered = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
 
-    def __repr__(self):
-        return ''.join([
-            'User ID: ', self.user_id, '\r\n',
-            'Order ID: ', self.id, '\r\n', self.content
-        ])
 
 class Product(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -52,11 +34,7 @@ class Product(db.Model):
     volume = db.Column(db.Float, nullable=False)
     size = db.Column(db.Float, nullable=False)
     price = db.Column(db.Float, nullable=False)
-    def __repr__(self):
-        return ''.join([
-            'Product ID: ', self.id, '\r\n',
-            'Name: ', self.name, '\r\n', self.content
-        ])
+    
 
 class Order_line(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -64,9 +42,3 @@ class Order_line(db.Model):
     order_id = db.Column(db.Integer, db.ForeignKey('order.id'), nullable=False)
     quantity = db.Column(db.Integer, nullable=False)
     total = db.Column(db.Float, nullable=False)
-
-    def __repr__(self):
-        return ''.join([
-            'Order Line ID: ', self.id, '\r\n',
-            'Product ID: ', self.product_id, '\r\n', self.content
-        ])
--- create.py
+++ create.py
@@ -1,5 +1,5 @@
 from application import db
-from application.models import Posts, Users, Product, Order_line, Order
+from application.models import Users, Product, Order_line, Order
 
 db.drop_all()
 db.create_all()
